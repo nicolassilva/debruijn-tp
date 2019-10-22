880f2dcd323bb4184da3e43520ac41b18fa97ca0
nicolasSilva <silva.nicolas.j@gmail.com>
2019-10-22 14:06:56 +0200

    Firt functions

--- debruij.py
+++ debruij.py
@@ -1,16 +1,47 @@
 import argparse
 
+def read_fastq(fastq_file):
+    """Read fastq and return list of sequence
+    Takes in arguments a FATSQ file"""
+    fq_file = open(fastq_file, 'r')
+    for line in fq_file:
+        yield next(fq_file)
+        next(fq_file)
+        next(fq_file)
+
+def cut_kmer(seq, kmer_size):
+    """Return list of Kmer
+    Takes in arguments a sequence and a Kmer size"""
+    for i in range(len(seq)-kmer_size):
+        yield seq[i:i+kmer_size]
+
+def build_kmer_dict(fastq_file, kmer_size):
+    """Return dictionnary of Kmer and occurency of the Kmer
+    Takes in arguments a FATSQ file and a Kmer size"""
+    kmer_dict = {}
+    for i in read_fastq(fastq_file):
+        for kmer in cut_kmer(i,kmer_size):
+            if not kmer in kmer_dict:
+                kmer_dict[kmer] = 1
+            else:
+                kmer_dict[kmer] += 1
+    return(kmer_dict)
+
 def main():
     """Main function"""
     parser = argparse.ArgumentParser(description='Graphe de de Bruijn')
-    i = parser.add_argument('-i', metavar='FASTQ', type=str, help='File FASTQ')
-    k = parser.add_argument('-k', metavar='Kmer', type=int, help='Kmer size', default='21')
-    o = parser.add_argument('-o', metavar='Confiq', type=str, help='Config file')
+    parser.add_argument('-i', metavar='FASTQ', type=str, help='File FASTQ', required=True)
+    parser.add_argument('-k', metavar='Kmer', type=int, help='Kmer size', default='21')
+    #parser.add_argument('-o', metavar='Confiq', type=str, help='Config file', required=True)
     args = parser.parse_args()
+    fastq_file = args.i
+    kmer_size = args.k
+    #config_file = args.o
+    
+    test = build_kmer_dict(fastq_file,kmer_size)
+    print(test)      
+    
+    return()
 
 if __name__ == '__main__':
-    #fast_file = i
-    kmer_size = k
-    #config_file = o
-    print(k)
     main()
